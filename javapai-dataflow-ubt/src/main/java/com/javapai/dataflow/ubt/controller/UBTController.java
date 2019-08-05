b88280e59377dfb1ca9d474035b32e9607d2d97f
yonghuxiang <xiang19999>
2019-08-05 11:37:14 +0800

    1：增加生产环境配置。 2：调整UBT接口代码。

--- javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/controller/UBTController.java
+++ javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/controller/UBTController.java
@@ -2,11 +2,18 @@ package com.javapai.dataflow.ubt.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,6 +21,8 @@ import com.javapai.dataflow.ubt.param.UBTRecord;
 
 @RestController
 public final class UBTController {
+	private static final Logger logger = LoggerFactory.getLogger(UBTController.class);
+	
 	/*默认UBT Topic Name*/
 	private static final String TOPIC_UBT = "DATAFLOW_TOPIC_UBT";
 	
@@ -23,21 +32,80 @@ public final class UBTController {
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
-	@RequestMapping("/ubt/ubtRecord")
-	public void ubtRecord(@RequestBody UBTRecord dto) {
+	@PostMapping("/ubt/ubtEvent")
+	public void ubtEvent(@RequestBody UBTRecord event) {
+		if(filterEvent(event)) {
+			//
+			long currentTime = System.currentTimeMillis();
+			if (event.getTimestamp() <= 0 || event.getTimestamp() > currentTime) {
+				event.setTimestamp(currentTime);
+			}
+			//
+			ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+			if (event.getProperty("$ip") == null) {
+				String ip = getRemoteAddr(requestAttributes.getRequest());
+				event.addProperty("$ip", ip);
+			}
+			//
+			try {
+				kafkaTemplate.send(TOPIC_UBT, objectMapper.writeValueAsString(event));
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@PostMapping("/ubt/ubtEventBatch")
+	public void ubtEventBatch(@RequestBody List<UBTRecord> dto) {
 		try {
 			kafkaTemplate.send(TOPIC_UBT, objectMapper.writeValueAsString(dto));
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	private boolean filterEvent(UBTRecord event) {
+		if (StringUtils.isEmpty(event.getAppId())) {
+			logger.error("UBT数据({}-{})的appId为空!");
+			// logger.error("UBT数据的appId为空" + objectMapper.writeValueAsString(event));
+			return false;
+		} else if (StringUtils.isEmpty(event.getSourceId())) {
+			logger.error("UBT数据({}-{})的distinctId为空!");
+			// logger.error("UBT数据的distinctId为空" + objectMapper.writeValueAsString(event));
+			return false;
+		} else if (StringUtils.isEmpty(event.getAction())) {
+			logger.error("UBT数据({}-{})的action为空!");
+			// logger.error("UBT数据的action为空" +  objectMapper.writeValueAsString(event));
+			return false;
+		}
+		return true;
+	}
+	
+	public static String getRemoteAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-Real-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
 
-	@RequestMapping("/ubt/ubtBatchRecord")
-	public void ubtBatchRecord(@RequestBody List<UBTRecord> dto) {
-		try {
-			kafkaTemplate.send(TOPIC_UBT, objectMapper.writeValueAsString(dto));
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+		logger.info("RemoteAddr: " + ip);
+		if (!StringUtils.isEmpty(ip)) {
+			String[] ipArray = ip.split(",");
+			if (ipArray != null && ipArray.length > 1) {
+				return ipArray[0];
+			}
+			return ip;
+		} else {
+			return ip;
 		}
 	}
 
--- javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/param/UBTRecord.java
+++ javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/param/UBTRecord.java
@@ -1,5 +1,122 @@
 package com.javapai.dataflow.ubt.param;
 
-public class UBTRecord {
+import java.util.HashMap;
+import java.util.Map;
+
+public final class UBTRecord {
+	/**
+	 * 项目名，必选项。<br>
+	 * <br>
+	 * <strong>根据现场情况自行定义.</strong>
+	 */
+	private String appId;
+
+	/**
+	 * 用户登录标识(注册账号).
+	 */
+	private String userId;
+
+	/**
+	 * 数据来源标识.<br>
+	 * <br>
+	 * 移动设备填写设备号，其它访问源填写cookiesId.
+	 */
+	private String sourceId;
+
+	/**
+	 * 用户跟目标交互的行为，必选项。<br>
+	 * <br>
+	 * <strong>根据现场情况自行定义.</strong>比如"点击"、"输入"、"跳转"、"进入"、"滑动"等等。
+	 */
+	private String action;
+
+	/**
+	 * 要监控的目标的事件名称，通常是同一组目标的名字，必选项。<br>
+	 * <br>
+	 * <strong>根据现场情况自行定义.</strong>比如"产品浏览"、"单期借款"、"借款银行卡"等等。
+	 */
+	private String event;
+
+	/**
+	 * 事件发生的实际时间戳，精确到毫秒；
+	 */
+	private long timestamp;
+
+	/**
+	 * 预留属性;
+	 */
+	private Map<String, String> properties;
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, String> properties) {
+		this.properties = properties;
+	}
+	
+	public void addProperty(String key, String value) {
+		if (properties == null) {
+			properties = new HashMap<String, String>();
+		} else {
+			properties.put(key, value);
+		}
+	}
+
+	public String getProperty(String key) {
+		if (properties == null) {
+			return null;
+		} else {
+			return properties.get(key);
+		}
+	}
 
 }
