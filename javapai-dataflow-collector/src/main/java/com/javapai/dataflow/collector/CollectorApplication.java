9559662789f47d280a12e0197e9eeeb5e76d48f3
liuxiang <liuxiang@sinafenqi.com>
2019-08-06 15:18:07 +0800

    初始化数据收集模块。

---
+++ javapai-dataflow-collector/src/main/java/com/javapai/dataflow/collector/CollectorApplication.java
@@ -0,0 +1,11 @@
+package com.javapai.dataflow.collector;
+
+/**
+ * Hello world!
+ *
+ */
+public class CollectorApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
