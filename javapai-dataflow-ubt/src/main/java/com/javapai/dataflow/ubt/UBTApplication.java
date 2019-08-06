1a9d8f9a2a498300888b563b5727abcdc63de1db
yonghuxiang <xiang19999>
2019-08-06 11:16:52 +0800

    UBT模型说明更新。

--- javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/UBTApplication.java
+++ javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/UBTApplication.java
@@ -1,12 +1,13 @@
 package com.javapai.dataflow.ubt;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @EnableAutoConfiguration
-public class App {
+public class UBTApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(UBTApplication.class, args);
 	}
 }
--- javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/param/UBTRecord.java
+++ javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/param/UBTRecord.java
@@ -3,9 +3,15 @@ package com.javapai.dataflow.ubt.param;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * UTB行为数据模型。<br>
+ * 
+ * @author pooja
+ *
+ */
 public final class UBTRecord {
 	/**
-	 * 项目名，必选项。<br>
+	 * 项目标识，必选项。<br>
 	 * <br>
 	 * <strong>根据现场情况自行定义.</strong>
 	 */
@@ -26,7 +32,7 @@ public final class UBTRecord {
 	/**
 	 * 用户跟目标交互的行为，必选项。<br>
 	 * <br>
-	 * <strong>根据现场情况自行定义.</strong>比如"点击"、"输入"、"跳转"、"进入"、"滑动"等等。
+	 * <strong>参考ActionEnum定义.</strong>
 	 */
 	private String action;
 
@@ -43,7 +49,48 @@ public final class UBTRecord {
 	private long timestamp;
 
 	/**
-	 * 预留属性;
+	 * UBT事件预留属性; <br>
+	 * <h4>系统级属性(约定前缀$开头，如$os)：</h4><br>
+	 * <blockquote>
+	 * <table border=0 cellspacing=3 cellpadding=0>
+	 * <tr style="background-color: rgb(204, 204, 255);">
+	 * <th align=left>-属性名-
+	 * <th align=left>-类型-
+	 * <th align=left>-描述-
+	 * <tr>
+	 * <td><code>$os</code>
+	 * <td><code>String</code>
+	 * <td><code>操作系统</code>
+	 * <tr>
+	 * <td><code>$osbanben</code>
+	 * <td><code>String</code>
+	 * <td><code>操作系统版本</code>
+	 * <tr>
+	 * <td><code>$appbanben</code>
+	 * <td><code>String</code>
+	 * <td><code>app版本</code>
+	 * <tr>
+	 * <td><code>$longitude</code>
+	 * <td><code>String</code>
+	 * <td><code>经度</code>
+	 * <tr>
+	 * <td><code>$latitude</code>
+	 * <td><code>String</code>
+	 * <td><code>纬度</code>
+	 * <tr>
+	 * <td><code>$appchannel</code>
+	 * <td><code>String</code>
+	 * <td><code>app下载渠道</code>
+	 * <tr>
+	 * <td><code>$marketchannel</code>
+	 * <td><code>String</code>
+	 * <td><code>活动推广渠道</code>
+	 * </tr>
+	 * </table>
+	 * </blockquote>
+	 * 
+	 * <br>
+	 * 业务级属性(用户自定义)
 	 */
 	private Map<String, String> properties;
 
@@ -102,7 +149,7 @@ public final class UBTRecord {
 	public void setProperties(Map<String, String> properties) {
 		this.properties = properties;
 	}
-	
+
 	public void addProperty(String key, String value) {
 		if (properties == null) {
 			properties = new HashMap<String, String>();
