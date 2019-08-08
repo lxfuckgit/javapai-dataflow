06f8913e5132a6e40f43953bbeb3ae663986c42c
yonghuxiang <xiang19999>
2019-08-08 15:25:08 +0800

    pom.xml依赖jar版本升级。

--- javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/UBTApplication.java
+++ javapai-dataflow-ubt/src/main/java/com/javapai/dataflow/ubt/UBTApplication.java
@@ -1,11 +1,11 @@
 package com.javapai.dataflow.ubt;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
+//@org.springframework.context.annotation.Configuration
+//@org.springframework.boot.autoconfigure.EnableAutoConfiguration
+@SpringBootApplication
 public class UBTApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UBTApplication.class, args);
