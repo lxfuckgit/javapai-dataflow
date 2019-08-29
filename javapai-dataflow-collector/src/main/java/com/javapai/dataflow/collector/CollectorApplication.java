e9d35de4aa85ffcadce10049b5f35159663d4934
yonghuxiang <xiang19999>
2019-08-29 10:58:43 +0800

    增加健康检查接口/health

--- javapai-dataflow-collector/src/main/java/com/javapai/dataflow/collector/CollectorApplication.java
+++ javapai-dataflow-collector/src/main/java/com/javapai/dataflow/collector/CollectorApplication.java
@@ -2,10 +2,18 @@ package com.javapai.dataflow.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CollectorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CollectorApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/health")
+	public String health() {
+		return "i'm here!";
 	}
 }
